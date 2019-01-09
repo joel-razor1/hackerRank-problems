7401ee91712bb74c4a5ad50cf957a113dcb107eb
joel-razor1 <joelabraham8857@gmail.com>
2019-01-09 14:40:22 +0530

    First Problem in hackerrank

---
+++ Problem Solving (Practice)/2Darray-hour-glasss.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+   int main(){
+
+       int ar[6][6], sum[20],i,j,count=0,largest;
+
+       for(i=0;i<6;i++){
+           for(j=0;j<6;j++){
+               scanf("%d",&ar[i][j]);
+           }
+       }
+
+       for(i=0;i<4;i++){
+           for(j=0;j<4;j++){
+             sum[count++] =ar[i][j]+ar[i][j+1]+ar[i][j+2]+ar[i+1][j+1]+ar[i+2][j]+ar[i+2][j+1]+ar[i+2][j+2];
+           }
+       }
+        largest=sum[0];
+        for(i=1;i<count;i++){
+            if(largest<sum[i]){
+                largest=sum[i];
+            }
+    
+        }
+        printf("%d",largest);
+
+       
+
+       
+
+
+
+       return 0;
+   }
+
